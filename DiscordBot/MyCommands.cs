b52a4d17f7bf0a4f6449f9204cdc2ba93115614c
Romenics <romenics@science-interactive.net>
2019-02-02 21:18:28 +0300

    Commands added

---
+++ DiscordBot/MyCommands.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+
+namespace DiscordBot {
+	public class MyCommands {
+
+		[Command("Ping")]
+		public async Task Ping (CommandContext context) {
+			await context.RespondAsync ("📡 Pong");
+		}
+
+		[Command ("Invite")]
+		public async Task Invite (CommandContext context) {
+			await context.RespondAsync ("https://discordapp.com/oauth2/authorize?client_id=531216002956918804&scope=bot&permissions=8");
+		}
+
+		[Command ("Help")]
+		public async Task Help (CommandContext context) {
+			string Respond = 
+			"```Commands: \n Help \n Ping \n Invite \n d \n Choose```";
+			await context.RespondAsync (Respond);
+		}
+
+		[Command("d")]
+		public async Task RollDice (CommandContext context, int Side) {
+			Random rand = new Random ();
+			Console.WriteLine (Side);
+			string Respond = "🎲 Rolled: " + rand.Next (1, Side);
+			await context.RespondAsync (Respond);
+		}
+
+		[Command ("Choose")]
+		public async Task Choose (CommandContext context) {
+			string[] Suggestions = context.Message.Content.Split(' ');
+			Random rand = new Random ();
+			int Answer = rand.Next (1, Suggestions.Length);
+			string Respond = "🤔 Hmm, I'm Choosing: " + Suggestions[Answer];
+			await context.RespondAsync (Respond);
+		}
+		
+	}
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -2,64 +2,67 @@
 using System.Threading;
 using System.Threading.Tasks;
 using DSharpPlus;
+using DSharpPlus.EventArgs;
+using DSharpPlus.CommandsNext;
 
 namespace DiscordBot {
-	class Program {
+
+	public class Program {
 		
 		static DiscordClient discord;
 
-		public static int PartyCount;
+		static CommandsNextModule commands;
 
+		public static int PartyCount;
+		public static string LastDeletedMessage;
+
+		
 		static void Main (string[] args) {
 			MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
 		}
 
 		static async Task MainAsync (string[] args) {
 			
+			//Настройка базовой конфигурации бота
 			DiscordConfiguration DiscordConfig = new DiscordConfiguration {
 				Token = "NTMxMjE2MDAyOTU2OTE4ODA0.Dy2k6w.Ly_zkEgU4Udq1jAJc_Mf0SugjUU",
-				TokenType = TokenType.Bot 
+				TokenType = TokenType.Bot,
+				UseInternalLogHandler = true, 
+				LogLevel = LogLevel.Debug
+				
 			};
 			
-			Console.WriteLine ("Bot staterted");
-
 			discord = new DiscordClient (DiscordConfig);
 
-			//Bot check
-			discord.MessageCreated += async e => {
-				if (e.Message.Content.ToLower().StartsWith("ping"))
-					await e.Message.RespondAsync("pong!");
+			//Настройка списка комманд
+			CommandsNextConfiguration commandsConfig = new CommandsNextConfiguration {
+				StringPrefix = "!!",
+				EnableMentionPrefix = true
 			};
 
-			//Roll Dice
-			discord.MessageCreated += async e => {
-				if (e.Message.Content.ToLower().StartsWith("d")) {
-					int dice = 0;
-					if (int.TryParse (e.Message.Content.TrimStart('d'), out dice) == true) {
-						dice = RollDice (dice);
-						await e.Message.RespondAsync ("Rolled " + dice);
-					}
-					else {
-						await e.Message.RespondAsync ("Input error");
-					}
-				}
-			};
+			commands = discord.UseCommandsNext (commandsConfig);
+			commands.RegisterCommands <MyCommands> ();
+			Console.WriteLine ("Bot staterted");
+
 
 			//Restore deleting message
 			discord.MessageDeleted += async e => {
-				string Respond = e.Message.Author.Username +" пытается удалить  " + e.Message.Content + "\n";
-				Respond += "Но у него не получается!";
-				await e.Message.RespondAsync (Respond);
+				LastDeletedMessage = e.Message.Content;
+				await e.Message.RespondAsync ();
 			};
 
 			//Working when readction added
-			discord.MessageReactionAdded += async e => {
-				if (e.Emoji.Name == "👍") {
-					await e.Message.RespondAsync ("Это палец");
-				}
-			};
+			discord.MessageReactionAdded += TumbUp;
+			discord.MessageReactionAdded += Clock;
+			
+			async Task TumbUp (MessageReactionAddEventArgs e) {
 
-			discord.MessageReactionAdded += async e => {
+				if (e.Emoji.Name == "👍") {
+					await e.Message.RespondAsync (e.User.Username + " like it!");
+				}
+			}
+
+			async Task Clock (MessageReactionAddEventArgs e) {
 
 				if (e.Emoji.Name == "⏲") {
 					string Respond = e.User.Username + " готов поиграть в ДС!";
@@ -68,60 +71,10 @@ namespace DiscordBot {
 					}
 					await e.Message.RespondAsync (Respond);
 				}
-
-				//System.Collections.Generic.IReadOnlyList<DSharpPlus.Entities.DiscordConnection> x = await e.Client.ge discord.GetConnectionsAsync ();
-				//for (int i = 0; i < x.Count; i++) {
-				//	Console.WriteLine ("Get connection");
-				//	if (x[i].Id == 370315299968516114) {
-				//
-				//	}
-				//}
-			};
+			}
 			
-			discord.ChannelUpdated += async e => {
-				Console.WriteLine ("Channel updated");
-				if (e.ChannelAfter.Id == 370315299968516114) {
-					Console.WriteLine ("User joined, now party size: " + PartyCount);
-					PartyCount ++;
-				}
-
-				if (e.ChannelBefore.Id == 370315299968516114) {
-					Console.WriteLine ("User left, now party size: " + PartyCount);
-					PartyCount --;
-				}
-			};
-
-		
-			
-			
-
-			discord.VoiceStateUpdated += async e => {
-				
-				Console.WriteLine ("Channel updated " + e.Channel.UserLimit);
-				Console.WriteLine (e.Channel.Position);
-				string Respond = e.User.Username + " updated " + e.Channel.Name;
-				if (e.Channel.Name.ToLower () == "таверна") {
-					//PartyCount = e
-				}
-			};
-
 			await discord.ConnectAsync();
-            await Task.Delay(-1);
-		}
-
-		public void StartTimer (double seconds) {
-			System.Timers.Timer timer = new System.Timers.Timer (seconds);
-			timer.Elapsed  += new System.Timers.ElapsedEventHandler (StartEvent);
-		}
-
-		public static void StartEvent (object source, System.Timers.ElapsedEventArgs e) {
-
-		}
-
-		public static int RollDice (int Side) {
-			Random rand = new Random();
-			Console.WriteLine ("Random " + rand.Next (1, Side));
-			return rand.Next (1, Side);
+			await Task.Delay(-1);
 		}
 	}
 }
