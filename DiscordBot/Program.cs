3f1f57da8a29a145cee031b63657d105860a8491
Romenics <romenics@science-interactive.net>
2020-01-20 01:06:30 +0300

    Token fix

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,9 +17,14 @@ namespace DiscordBot {
 
 		public static int PartyCount;
 		public static string LastDeletedMessage;
+		//Загружается из файла Token.txt
+		public static string DiscordToken;
 
 		
 		static void Main (string[] args) {
+
+			DiscordToken = ReadTxt ("Token.txt");
+			Console.WriteLine ("You Token: " + DiscordToken);
 			MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
 		}
 
@@ -26,7 +32,7 @@ namespace DiscordBot {
 			
 			//Настройка базовой конфигурации бота
 			DiscordConfiguration DiscordConfig = new DiscordConfiguration {
-				Token = "NTMxMjE2MDAyOTU2OTE4ODA0.Dy2k6w.Ly_zkEgU4Udq1jAJc_Mf0SugjUU",
+				Token = DiscordToken,
 				TokenType = TokenType.Bot,
 				UseInternalLogHandler = true, 
 				LogLevel = LogLevel.Debug
@@ -295,5 +301,41 @@ namespace DiscordBot {
 
 			await Task.Delay(-1);
 		}
+	
+		
+
+		/// <summary>
+		/// Выдает содержимое каждой строки txt файла который лежит в корне Unity проекта (рядом с Assets)
+		/// Имя файла например Test (без расширения)
+		/// </summary>
+		public static string[] ReadTxtLines (string FileName) {
+
+			string Path = @"./" + FileName + ".txt";
+
+
+			if (File.Exists (Path) == false) {
+				return new string[0];
+			}
+			else {
+				return File.ReadAllLines (Path);
+			}
+		}
+
+
+		/// Выдает содержимое txt файла который лежит в корне Unity проекта (рядом с Assets)
+		/// Имя файла например Test.txt
+		public static string ReadTxt (string FileName) {
+
+			string Path = @"./" + FileName;
+
+
+			if (File.Exists (Path) == false) {
+				return string.Empty;
+			}
+			else {
+				return File.ReadAllText (Path);
+			}
+		}
+
 	}
 }
